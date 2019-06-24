51fe32719114538899adec47be26699837dd3abc
whopdan <whop.dan@gmail.com>
2019-06-23 19:00:30 -0500

    Update handlers.go

--- handlers.go
+++ handlers.go
@@ -1,6 +1,7 @@
 package wrkrcom
 
 import (
+	"errors"
 	"log"
 	"net/http"
 	"time"
@@ -11,18 +12,44 @@ import (
 	"github.com/globalsign/mgo"
 )
 
+// ErrClientNotSet indicates that a Faktory client has not been set
+var ErrClientNotSet = errors.New("Faktory client not set")
+
 // Domain houses data provided to a handler
 type Domain struct {
-	Mgo       *mgo.Session
-	MgoDB     *mgo.Database
-	FakClient *faktory.Client
+	Mgo          *mgo.Session
+	MgoDB        *mgo.Database
+	FakClientOK  bool
+	FakClient    *faktory.Client
+	FakClientErr error
+}
+
+// NewDomain constructs a domain object
+func NewDomain() (Domain, error) {
+	var rtn Domain
+
+	rtn.FakClient, rtn.FakClientErr = faktory.Open()
+	if rtn.FakClientErr == nil {
+		// successfully created a Faktory client
+		rtn.FakClientOK = true
+	}
+	return rtn, rtn.FakClientErr
 }
 
 // HndlrStatus returns the status of the Faktory client
-func (dmn *Domain) HndlrStatus(c *gin.Context) {
+func (dmn Domain) HndlrStatus(c *gin.Context) {
 	var err error
 	var tMap = make(map[string]interface{})
 	var rMap = make(map[string]interface{})
+
+	// Has a Faktory client object been generated and
+	if !dmn.FakClientOK {
+		// Faktory client error has occurred
+		rMap["msg"] = "Faktory client error has occurred"
+		rMap["err"] = dmn.FakClientErr
+		c.JSON(http.StatusInternalServerError, rMap)
+		return
+	}
 
 	tMap, err = dmn.FakClient.Info()
 	if err != nil {
@@ -36,8 +63,37 @@ func (dmn *Domain) HndlrStatus(c *gin.Context) {
 	rMap["content"] = tMap
 }
 
+// GenerateHandlrStatus return a gin handler
+func (dmn Domain) GenerateHandlrStatus() gin.HandlerFunc {
+	return func(c *gin.Context) {
+		var err error
+		var tMap = make(map[string]interface{})
+		var rMap = make(map[string]interface{})
+
+		// Has a Faktory client object been generated and
+		if !dmn.FakClientOK {
+			// Faktory client error has occurred
+			rMap["msg"] = "Faktory client error has occurred"
+			rMap["err"] = dmn.FakClientErr
+			c.JSON(http.StatusInternalServerError, rMap)
+			return
+		}
+
+		tMap, err = dmn.FakClient.Info()
+		if err != nil {
+			rMap["msg"] = "error occurred querying the Faktory client"
+			rMap["err"] = err.Error()
+			c.JSON(http.StatusInternalServerError, rMap)
+			return
+		}
+
+		rMap["msg"] = "response from the Faktory client"
+		rMap["content"] = tMap
+	}
+}
+
 // DummyFunc executes a dummy worker function
-func (dmn *Domain) DummyFunc(ctx worker.Context, args ...interface{}) error {
+func (dmn Domain) DummyFunc(ctx worker.Context, args ...interface{}) error {
 	log.Println("INFO: executing DummyFunc at time:", time.Now().Format(time.RFC3339))
 	return nil
 }
